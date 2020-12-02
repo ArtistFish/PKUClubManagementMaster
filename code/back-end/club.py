75363e6cc030a989ca6d172f5da54f70bf7ba1b0
ArtistFish <1800011012@pku.edu.cn>
2020-12-02 23:46:25 +0800

    完成了clubs类的数据库内容，并实现了getClubInfo的简易接口

--- code/back-end/club.py
+++ code/back-end/club.py
@@ -1,112 +1,92 @@
-'''
-Temporarily use sqlite3 for debug
-Probably can satisfy our needs
-'''
-import sqlite3
-#import mysql.connector
+import mysql.connector
+
 
 class Club():
     def __init__():
-        '''
-        not used
-        '''
-        self.president = new_president
-        self.managerList=[]
-        self.memberList=[]
-        self.activityList = []
+        pass
 
-    def createClub(new_id,new_name,new_president='',new_description='',new_ico='',new_picture=''):
-        conn = sqlite3.connect('test.db')
+
+    def createClub(club_id,club_name,club_description='',club_president_user_id=0):
+        conn = mysql.connector.connect(user='root', password='root', database='test_gp10')
         cursor = conn.cursor()
-        str='insert into clubs (id, name, description,president,ico,picture) values (%d, \'%s\',\'%s\',\'%s\',\'%s\',\'%s\')'%(new_id,new_name,new_description,new_president,new_ico,new_picture)         
-        #社团基本信息
-        cursor.execute(str) 
-        res=cursor.rowcount
-        
 
-        str='create table club%dactivitylist (acti_id int)'%(new_id) #社团活动表，记录活动编号
-        cursor.execute(str)
-        str='create table club%dmemberlist (member_id varchar)'%(new_id)#社团成员表，记录成员微信号
-        cursor.execute(str)
-        str='create table club%dmanagerlist (manager_id varchar)'%(new_id)#社团管理者表，记录管理者微信号
-        cursor.execute(str)
+
+        try:
+            cursor.execute("insert into clubs (club_id,club_name,club_description,club_president_user_id) values (%d,'%s','%s',%d)"%(club_id,club_name,club_description,club_president_user_id))
+        except mysql.connector.errors.IntegrityError:  #Duplicate entry for key 'PRIMARY'
+            return 1
+        
+        try:
+            cursor.execute("create table club_%d_activities (activity_id INT primary key)"%(club_id))
+        except mysql.connector.errors.ProgrammingError: #table already exist
+            pass
+
+        try:
+            cursor.execute("create table club_%d_members (member_user_id INT primary key)"%(club_id))
+        except mysql.connector.errors.ProgrammingError:
+            pass
+
+        try:
+            cursor.execute("create table club_%d_managers (manager_user_id INT primary key)"%(club_id))
+        except mysql.connector.errors.ProgrammingError:
+            pass
 
         cursor.close()
         conn.commit()
         conn.close()
-        return res
+        return 0
 
     def updateClubInfo(new_id,new_name,new_prisident='',new_description='',new_ico='',new_picture=''):
-        conn = sqlite3.connect('test.db')
-        cursor = conn.cursor()
-        str='update clubs set name=\'%s\',description=\'%s\',president=\'%s\',ico=\'%s\',picture=\'%s\' where id=%d'%(new_name,new_description,id)
-        cursor.execute(str)
-        res=cursor.rowcount
-
-        cursor.close()
-        conn.commit()
-        conn.close()
-        return res
+        pass
        
 
     def deleteClub(club_id):
-        conn = sqlite3.connect('test.db')
+        conn = mysql.connector.connect(user='root', password='root', database='test_gp10')
         cursor = conn.cursor()
-        str='delete from clubs where id=%d'%(club_id)
-        cursor.execute(str)
-        str='drop table club%dactivitylist'%(club_id)
-        cursor.execute(str)
-        str='drop table club%dmemberlist'%(club_id)
-        cursor.execute(str)
-        res=cursor.rowcount
+        try:
+            cursor.execute('delete from clubs where club_id=%d'%(club_id))
+        except mysql.connector.errors.ProgrammingError:
+            return 1
+
+        try:
+            cursor.execute("delete table club_%d_activities"%(club_id))
+        except mysql.connector.errors.ProgrammingError: 
+            return 2
+
+        try:
+            cursor.execute("delete table club_%d_members"%(club_id))
+        except mysql.connector.errors.ProgrammingError: 
+            return 3
+
+        try:
+            cursor.execute("delete table club_%d_managers"%(club_id))
+        except mysql.connector.errors.ProgrammingError: 
+            return 4
+
 
         cursor.close()
         conn.commit()
         conn.close()
-        return res
+        return 0
 
-    def getClubInfo(club_id):
-        conn = sqlite3.connect('test.db')
+
+
+    def getClubInfo(club_id): #return a list of tuples
+        conn = mysql.connector.connect(user='root', password='root', database='test_gp10')
         cursor = conn.cursor()
-        str='select * from clubs where id=%d'%(club_id)
-        cursor.execute(str)
+
+        try:
+            cursor.execute("select * from clubs where club_id=%d"%(club_id))
+        except mysql.connector.errors.ProgrammingError:       
+            pass
+
         res = cursor.fetchall()
-        #print(res)
         cursor.close()
         conn.close()
         return res
 
 
 
-    def addManager(self,new_manager):
-        if not new_manager in self.managerList:
-            self.managerList.append(new_manager)
-    
-    def deleteManager(self,old_manager):
-        if old_manager in self.managerList:
-            self.managerList.remove(old_manager)
-
-    def addMember(self,new_member):
-        if not new_member in self.memberList:
-            self.memberList.append(new_member)
-    
-    def deleteMember(self,old_member):
-        if old_member in self.memberList:
-            self.memberList.remove(old_member)
-
-    def addActivity(self,new_activity):
-        if not new_activity in self.activityList:
-            self.activityList.append(new_activity)
-    
-    def deleteActivity(self,old_activity):
-        if old_activity in self.activityList:
-            self.activityList.remove(old_activity)
-
-    def changeDescription(self,new_description):
-        self.description=new_description
-
-    def sendNotice(self):
-        pass
     
 
 
