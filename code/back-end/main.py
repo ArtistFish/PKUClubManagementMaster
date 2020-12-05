e9895eea5f21681a6632003bd42108b18d1a552e
ArtistFish <56578861+ArtistFish@users.noreply.github.com>
2020-12-05 23:16:10 +0800

    Add files via upload
    实现了club类的基本功能
    修改了datamanager的部分问题
    datamanager类对于club_id_members club_id_manager的数据库操作尚未实现
    所有代码已在服务器上测试过

--- code/back-end/main.py
+++ code/back-end/main.py
@@ -1,45 +1,42 @@
 from flask import Flask, render_template, request, json
-import mysql.connector
 from club import Club
-import sys
-import os
+from datamanager import *
 
 
 app = Flask(__name__)
 
 
-# just for test
+# for test
 @app.route("/gp10",methods=['GET'])
-def testc_form():
+def test_form():
     return "response from SE group 10"
 
 '''
 API:
 create a club
 '''
-@app.route('/gp10/createClub_test/<club_id>/<club_name>', methods=['POST', 'GET'])
-def createClub_test(club_id, club_name):
-    res=Club.createClub(club_id=int(club_id),club_name=club_name,club_description='For test')
-    if res==0:
-        status='success'
-    else:
-        status='failed'
-    res={'status':status}
+@app.route('/gp10/createClub_test/<club_name>', methods=['POST', 'GET'])
+def createClub_test(club_name):
+    club_president_wxid='0x1'
+    club_description='description for test'
+    club=Club(club_name=club_name,club_description=club_description,club_president_wxid=club_president_wxid)
+    manager=DataManager(DataType.club)
+    manager.addInfo(club)
+
+    res={'status':'200 OK'}
     return json.dumps(res)
 
 @app.route('/gp10/createClub', methods=['POST'])
 def createClub():
-    club_id= int(json.loads(request.values.get("club_id")))
     club_name= str(json.loads(request.values.get("club_name")))
     club_description=str(json.loads(request.values.get("club_description")))
-    club_president_user_id=int(json.loads(request.values.get("club_president_user_id")))
+    club_president_wxid=int(json.loads(request.values.get("club_president_user_id")))
 
-    res=Club.createClub(club_id,club_name,club_description,club_president_user_id)
-    if res==0:
-        status='success'
-    else:
-        status='failed'
-    res={'status':status}
+    club=Club(club_name=club_name,club_description=club_description,club_president_wxid=club_president_wxid)
+    manager=DataManager(DataType.club)
+    manager.addInfo(club)
+
+    res={'status':'200 OK'}
     return json.dumps(res)
 
 
@@ -53,68 +50,27 @@ Return in Json format
 '''
 @app.route('/gp10/getClubInfo_test/<club_id>', methods=['POST', 'GET'])
 def getClubInfo_test(club_id):
-    data = Club.getClubInfo(int(club_id))
-    if len(data)==0:        # no club found
-        status='failed'
-        club_name=''
-        club_description=''
-        club_president_user_id=-1
-    else:
-        status='success'
-        club_name=data[0][1]
-        club_description=data[0][2]
-        club_president_user_id=data[0][3]
-    res = {'status':status,'club_name': club_name, 'club_description':club_description,'club_president_user_id':club_president_user_id}
-    return json.dumps(res)
+    club_id=int(club_id)
+    manager=DataManager(DataType.club)
+    club_info=manager.getInfo(club_id)
+    if len(club_info)==0:
+        return json.dumps({'status:':'Not Found'})
+    club=Club(club_id=club_id,club_name=club_info[0][1],club_description=club_info[0][2],club_president_wxid=club_info[0][3])
+    res=club.Jsonfy()
+    return res
 
 @app.route('/gp10/getClubInfo', methods=['POST'])
 def getClubInfo():
     club_id= int(json.loads(request.values.get("club_id")))
-    data = Club.getClubInfo(int(club_id))
-    if len(data)==0:        # no club found
-        status='failed'
-        club_name=''
-        club_description=''
-        club_president_user_id=-1
-    else:
-        status='success'
-        club_name=data[0][1]
-        club_description=data[0][2]
-        club_president_user_id=data[0][3]
-    res = {'status':status,'club_name': club_name, 'club_description':club_description,'club_president_user_id':club_president_user_id}
-    return json.dumps(res)
-
-def initSQL():
-    conn = mysql.connector.connect(user='root', password='root', database='test_gp10')
-    cursor = conn.cursor()
-
-        
-    try:
-        cursor.execute('create table users (user_id INT primary key,user_name TINYTEXT)')
-    except mysql.connector.errors.ProgrammingError:                     #table already exist
-        pass
-
-    try:
-        cursor.execute('create table clubs (club_id INT primary key, club_name TINYTEXT,club_description TEXT,club_president_user_id INT)')
-    except mysql.connector.errors.ProgrammingError:
-        pass
-
-    try:
-        cursor.execute('create table activities (activity_id INT primary key, activity_name TINYTEXT,activity_description TEXT,activity_club_id INT,activity_place TEXT,activity_start_time DATETIME,activity_end_time DATETIME,activity_lottery_time DATETIME,activity_lottery_method TINYTEXT,activity_max_number INT)')
-    except mysql.connector.errors.ProgrammingError:
-        pass
-
-    try:
-        cursor.execute('create table messages (message_id INT primary key,message_type TINYTEXT,message_title TINYTEXT,message_content TEXT,message_sender_user_id INT,message_receiver_user_id INT)')
-    except mysql.connector.errors.ProgrammingError:
-        pass
-
-    cursor.close()
-    conn.commit()
-    conn.close()
+    manager=DataManager(DataType.club)
+    club_info=manager.getInfo(club_id)
+    if len(club_info)==0:
+        return json.dumps({'status:':'Not Found'})
+    club=Club(club_id=club_id,club_name=club_info[0][1],club_description=club_info[0][2],club_president_wxid=club_info[0][3])
+    res=club.Jsonfy()
+    return res
 
 
 
 if __name__ == '__main__':
-    initSQL()
     app.run(debug=True, threaded=True,host='0.0.0.0')
