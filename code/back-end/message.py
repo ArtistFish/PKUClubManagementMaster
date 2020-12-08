1d07b4f6ca9c615daea6daf1e8db1e9270912691
WebboHsu <webbohsu@126.com>
2020-12-08 23:35:19 +0800

    调整DataManager类部分内容，以修复一些数据库访问错误。调整message.py部分内容。

--- code/back-end/message.py
+++ code/back-end/message.py
@@ -2,7 +2,7 @@ from flask import json
 from datamanager import *
 
 class Message():
-    __slots__ = ('id', 'type', 'title', 'content', 'sender_wxid', 'receiver_wxid')
+    #__slots__ = ('id', 'type', 'title', 'content', 'sender_wxid', 'receiver_wxid')
 
     def __init__(self, message_id = 0, message_type = '', message_title = '', message_content = '',
                  message_sender_wxid = '', message_receiver_wxid = ''):
@@ -14,10 +14,10 @@ class Message():
         self.receiver_wxid = message_receiver_wxid
 
 class MessageListOfUser():
-    __slots__ = {'status', 'send_message_list', 'receive_message_list'}
+    #__slots__ = {'status', 'send_message_list', 'receive_message_list'}
 
     def __init__(self, wxid):
         self.status = '200 OK'
         datamanager = DataManager(DataType.message)
-        send_message_list = datamanager.getList(wxid=wxid, flag=1)
-        receive_message_list = datamanager.getList(wxid=wxid, flag=2)
+        self.send_message_list = datamanager.getList(wxid=wxid, flag=1)
+        self.receive_message_list = datamanager.getList(wxid=wxid, flag=2)
