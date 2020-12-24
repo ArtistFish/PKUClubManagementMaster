cb41aa56141cfc18f2d194825989da92615bac06
even <ypx@pku.edu.cn>
2020-12-25 01:28:55 +0800

    修改message内的方法，为新字段做准备

--- code/back-end/message.py
+++ code/back-end/message.py
@@ -6,12 +6,17 @@ class Message():
 
     def __init__(self, message_id = 0, message_type = '', message_title = '', message_content = '',
                  message_sender_wxid = '', message_receiver_wxid = ''):
+                 #message_sender_wxid = '', message_receiver_wxid = '', message_isRead = False):
         self.id = message_id
         self.type = message_type
         self.title = message_title
         self.content = message_content
         self.sender_wxid = message_sender_wxid
         self.receiver_wxid = message_receiver_wxid
+        #self.isRead = message_isRead
+    
+    #def changeIsRead(self, new_state = True):
+    #    self.isRead = new_state
 
 class MessageListOfUser():
     #__slots__ = {'status', 'send_message_list', 'receive_message_list'}
